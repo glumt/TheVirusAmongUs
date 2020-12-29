1f09bc1d60b287c8b54e4ab4f01e65025200b274
Spiony <marucs.wiens@gmx.de>
2020-12-29 21:42:44 +0100

    Code formatting and replace scenes by class implementation

--- app.js
+++ app.js
@@ -13,39 +13,39 @@ const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
- 
+
 const players = {};
- 
-io.on('connection', function (socket) {
-  console.log('a user connected: ', socket.id);
-  // create a new player and add it to our players object
-  players[socket.id] = {
-    flipX: false,
-    x: Math.floor(Math.random() * 400) + 50,
-    y: Math.floor(Math.random() * 500) + 50,
-    playerId: socket.id
-  };
-  // send the players object to the new player
-  socket.emit('currentPlayers', players);
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
- 
-  // when a player disconnects, remove them from our players object
-  socket.on('disconnect', function () {
-    console.log('user disconnected: ', socket.id);
-    delete players[socket.id];
-    // emit a message to all players to remove this player
-    io.emit('disconnect', socket.id);
-  });
- 
-  // when a plaayer moves, update the player data
-  socket.on('playerMovement', function (movementData) {
-    players[socket.id].x = movementData.x;
-    players[socket.id].y = movementData.y;
-    players[socket.id].flipX = movementData.flipX;
-    // emit a message to all players about the player that moved
-    socket.broadcast.emit('playerMoved', players[socket.id]);
-  });
+
+io.on('connection', function(socket) {
+	console.log('a user connected: ', socket.id);
+	// create a new player and add it to our players object
+	players[socket.id] = {
+		flipX: false,
+		x: Math.floor(Math.random() * 400) + 50,
+		y: Math.floor(Math.random() * 500) + 50,
+		playerId: socket.id
+	};
+	// send the players object to the new player
+	socket.emit('currentPlayers', players);
+	// update all other players of the new player
+	socket.broadcast.emit('newPlayer', players[socket.id]);
+
+	// when a player disconnects, remove them from our players object
+	socket.on('disconnect', function() {
+		console.log('user disconnected: ', socket.id);
+		delete players[socket.id];
+		// emit a message to all players to remove this player
+		io.emit('disconnect', socket.id);
+	});
+
+	// when a plaayer moves, update the player data
+	socket.on('playerMovement', function(movementData) {
+		players[socket.id].x = movementData.x;
+		players[socket.id].y = movementData.y;
+		players[socket.id].flipX = movementData.flipX;
+		// emit a message to all players about the player that moved
+		socket.broadcast.emit('playerMoved', players[socket.id]);
+	});
 });
 
 // update express settings
@@ -61,9 +61,9 @@ app.get('/game.html', passport.authenticate('jwt', { session : false }), functio
 });
 */
 
-app.get('/game.html', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-  //res.sendFile(__dirname + '/index.html');
+app.get('/game.html', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+	//res.sendFile(__dirname + '/index.html');
 });
 
 
@@ -72,16 +72,16 @@ app.use(express.static(__dirname + '/public'));
 
 // catch all other routes
 app.use((req, res, next) => {
-  res.status(404).json({ message: '404 - Not Found' });
+	res.status(404).json({ message: '404 - Not Found' });
 });
 
 // handle errors
 app.use((err, req, res, next) => {
-  console.log(err.message);
-  res.status(err.status || 500).json({ error: err.message });
+	console.log(err.message);
+	res.status(err.status || 500).json({ error: err.message });
 });
 
 // have the server start listening on the provided port
 server.listen(process.env.PORT || 3000, () => {
-  console.log(`Server started on port ${process.env.PORT || 3000}`);
+	console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
